4979395788f8c4625638b0c3e80b21fbe75bafc9
sf0628 <148008966+sf0628@users.noreply.github.com>
2024-07-18 21:15:53 -0500

    delete duplicate footer message

--- client/src/components/Links.tsx
+++ client/src/components/Links.tsx
@@ -4,7 +4,6 @@ interface LinksProps {
 function Links({}: LinksProps) {
     return (
         <div className="footer-content">
-            <p>Developed by Sophia Fu, built in React and Express</p>
             <a href="www.linkedin.com/in/fusophia">
                 <img src="https://placehold.co/15x15"></img>
                 LinkedIn
